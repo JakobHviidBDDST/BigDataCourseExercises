90c4e709f820890dc55810eab9b170d1c53903d2
TidosDK <git@msnn.dev>
2026-09-06 19:17:47 +0200

    Updated sent_msg_students.py with new format of kubeconfigs and removed breaking assert

--- infrastructure/share_kubeconfigs/sent_msg_students.py
+++ infrastructure/share_kubeconfigs/sent_msg_students.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from datetime import datetime
 
 from dotenv import load_dotenv
 from src.msg import EmailClient, SMTPServer
@@ -11,8 +12,7 @@ if __name__ == "__main__":
     attachment_path = Path(
         os.getenv("KUBECONFIGS_DIR", Path(__file__).resolve().parent.parent / "tmp")
     )
-    attachment_files = list(attachment_path.glob("bd-stud-*.yaml"))
-    assert len(attachment_files) == 56, len(attachment_files)
+    attachment_files = list(attachment_path.glob("*-personal-sa-*.yaml"))
 
     ec = EmailClient(
         email=os.getenv("EMAIL", "<email>"),
@@ -20,15 +20,17 @@ if __name__ == "__main__":
         smtp_server=SMTPServer.SDU,
     )
 
+    current_year = datetime.now().strftime("%y")
+
     for attachment in attachment_files:
 
-        user_name = attachment.name.split("-")[-2]
+        user_name = attachment.name.split("-")[0]
         receiver_email: str = user_name + "@student.sdu.dk"
 
         msg = ec.create_msg(
             receiver_email=receiver_email,
-            subject="[Kubeconfig] - Kubeconfig for exercises in Big Data and Data Science Technology, E25",
-            body=f"Dear {user_name},\n\nHere is the kubeconfig file for the Kubernetes cluster you need for exercises in the course Big Data and Data Science Technology, E25.\n\nBest regards,\nAnders Launer Bæk-Petersen\n\n",
+            subject=f"[Kubeconfig] - Kubeconfig for exercises in Big Data and Data Science Technology, E{current_year}",
+            body=f"Dear {user_name},\n\nHere is the kubeconfig file for the Kubernetes cluster which you will need for exercises in the Big Data and Data Science Technology course, E{current_year}.\n",
             attachment=attachment,
         )
         # ec.send_msg(receiver_email, msg)
